b64676a6f9ff0c3a88b22dde2bb1d49411e2e265
ZulfiyaKurmanaeva <114097622+ZulfiyaKurmanaeva@users.noreply.github.com>
2022-12-13 07:40:58 +0300

    13/12/ 09.40

--- scripts/index.js
+++ scripts/index.js
@@ -2,14 +2,23 @@ const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const saveButton = document.querySelector('.popup__form');
 const closeButton = document.querySelector('.popup__close-button');
+const addButton = document.querySelector('.profile__add-button');
+
 let inputName = document.querySelector('.popup__input_type_name');
 let inputAbout = document.querySelector('.popup__input_type_about');
 let infoName = document.querySelector('.profile__name');
 let infoAbout = document.querySelector('.profile__about');
 
+let inputElementTitle = document.querySelector('.popup__input_type_ElementTitle');
+let inputElementLink = document.querySelector('.popup__input_type_ElementLink');
+let infoElementTitle = document.querySelector('.elements__title');
+//let ElementLink = ;
+
 editButton.addEventListener('click', openPopup);
 saveButton.addEventListener('submit', saveForm);
 closeButton.addEventListener('click', closePopup);
+addButton.addEventListener('click', openPopup);
+
 
 
 //popup.addEventListener('click', function (event) {
@@ -37,3 +46,31 @@ function saveForm(evt) {
 function closePopup() {
     popup.classList.remove('popup_opened');
 }
+
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 

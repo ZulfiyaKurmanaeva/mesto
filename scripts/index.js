06c1cc7177e155853c1b73e24cbbbf8c94203728
Zulfiya Kurmanaeva <kurzul13@gmail.com>
2022-12-19 03:20:10 +0500

    mesto 19/12/22 03.40

--- scripts/index.js
+++ scripts/index.js
@@ -1,143 +1,143 @@
-// cards
-
 const initialCards = [
   {
-    elementsTitle: 'Архыз',
-    elementsLink: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: 'Мост Золотые Ворота',
+    link: 'https://images.unsplash.com/photo-1664319744952-fc688e23c988?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
   },
   {
-    elementsTitle: 'Челябинская область',
-    elementsLink: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: 'Венеция',
+    link: 'https://images.unsplash.com/photo-1666880780155-81bd7ff1008a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
   },
   {
-    elementsTitle: 'Иваново',
-    elementsLink: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: 'Микены',
+    link: 'https://images.unsplash.com/photo-1670613873482-fdb5ea2fa6dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
   },
   {
-    elementsTitle: 'Камчатка',
-    elementsLink: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: 'Стокгольм',
+    link: 'https://images.unsplash.com/photo-1667382988243-6a1e5e85224e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
   },
   {
-    elementsTitle: 'Холмогорский район',
-    elementsLink: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: 'Тулуза',
+    link: 'https://images.unsplash.com/photo-1669854336002-0a2f4721dd5f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
   },
   {
-    elementsTitle: 'Байкал',
-    elementsLink: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    name: 'Нью-Йорк',
+    link: 'https://images.unsplash.com/photo-1669058002610-f325ddddf4d6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
   }
 ];
 
-//buttons
-const editProfileButton = document.querySelector('.profile__edit-button');
-const addElementsButton = document.querySelector('.profile__add-button');
-const saveProfileButton = document.querySelector('.popup__save_type_profile');
-const saveElementsButton = document.querySelector('.popup__save_type_element');
-const closeButton = document.querySelector('.popup__close-button');
-
-//popup
+//POPUP
 const popup = document.querySelector('.popup');
-const popupEditProfile = document.querySelector('.popup_type_edit-profile');
-const popupAddElements = document.querySelector('.popup_type_add-elements');
-const popupBigPicture = document.querySelector('.popup_type_big-picture');
+const closeButton = document.querySelector('.popup__close-button');
+const closeElementButton = document.querySelector('.popup__close-button_type_elements');
+const closeProfileButton = document.querySelector('.popup__close-button_type_profile');
+const closePictureButton = document.querySelector('.popup__close-button_type_picture');
 
-//profile
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+}
+
+closeElementButton.addEventListener('click', () => {
+  closePopup(popupAddElements);
+});
+closeProfileButton.addEventListener('click', () => {
+  closePopup(popupEditProfile);
+});
+closePictureButton.addEventListener('click', () => {
+  closePopup(popupBigPicture);
+});
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+//PROFILE
+const popupEditProfile = document.querySelector('.popup_type_edit-profile');
+const profileEditButton = document.querySelector('.profile__edit-button');
 const inputName = document.querySelector('.popup__input_type_name');
 const inputAbout = document.querySelector('.popup__input_type_about');
 const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__about');
-
-//Elements
-const inputElementTitle = document.querySelector('.popup__input_type_element-title');
-const inputElementLink = document.querySelector('.popup__input_type_element-link');
-const elementsTitle = document.querySelector('.elements__title');
-const elementsLink = document.querySelector('.elements__link');
-
-//container
-const elementsContainer = document.querySelector('.elements');
-const elementsTemplate = document.querySelector('#elementsTemplate');
-const elementsLikeBtn = elementsItem.querySelector('.elements__like-button');
-const elementsDelete = elementsItem.querySelector('.elements__delete-button');
-
-//popup image
-const bigPicture = document.querySelector('.popup__picture');
-const bigPictureCaption = document.querySelector('.popup__picture-caption');
-
-// open popup
-function openPopup() {
-  popup.classList.add('popup_opened');
-}
-
-//close popup
-closeButton.addEventListener('click', () => {
-  popup.classList.remove('popup_opened');
-})
-
-//edit profile
-editProfileButton.addEventListener('click', () => {
-  openPopup(popupEditProfile);
+//open edit profile
+profileEditButton.addEventListener('click', () => {
   inputName.value = profileName.textContent;
-  inputAbout.value = profileAbout.textContent;  
+  inputAbout.value = profileAbout.textContent;
+  openPopup(popupEditProfile);
 });
 
 //save profile
-saveProfileButton.addEventListener('submit', (evt) => {
+popupEditProfile.addEventListener('submit', (evt) => {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileAbout.textContent = inputAbout.value;
-  closePopup();
+  closePopup(popupEditProfile);
 })
 
-//edit/add elements
-addElementsButton.addEventListener('click', () => {
+//ELEMENTS
+const popupAddElements = document.querySelector('.popup_type_add-elements'); //elements form
+const profileAddButton = document.querySelector('.profile__add-button');
+const elementsContainer = document.querySelector('.elements');
+const elementsTemplate = document.querySelector('#elementsTemplate');
+
+const titleInput = document.querySelector('.popup__input_type_elements-title');
+const linkInput = document.querySelector('.popup__input_type_elements-link');
+
+const popupBigPicture = document.querySelector('.popup_type_big-picture');
+const bigPicture = document.querySelector('.popup__big-picture');
+const bigPictureCaption = document.querySelector('.popup__picture-caption');
+
+//open Elements popup
+profileAddButton.addEventListener('click', () => {
   openPopup(popupAddElements)
-  inputElementTitle.value = elementsTitle.textContent;
-  inputElementLink.value = elementsLink.src; 
 });
 
+//submit Elements
+popupAddElements.addEventListener('submit', (evt) => {
+  evt.preventDefault()
+  const link = linkInput.value;
+  const name = titleInput.value;
+  renderElements({ name, link });
+  linkInput.value = "";
+  titleInput.value = "";
+  closePopup(popupAddElements);
+  popupAddElements.reset();
+})
 
-//save elements
-saveElementsButton.addEventListener('click', () => {
-createItem(elementsTitle, elementsLink);
-evt.preventDefault();
-});
+//create Elements
+const createItems = ({ name, link }) => {
+  const elementsItem = elementsTemplate.content.querySelector('.elements__item').cloneNode(true);
+  const imageLink = elementsItem.querySelector('.elements__image');
+  const imageTitle = elementsItem.querySelector('.elements__title');
+  imageLink.src = link;
+  imageTitle.textContent = name;
+  imageLink.alt = name;
 
-//create element
-function createItem(elementsTitle, elementsLink) {
-const elementsItem = elementsTemplate.content.querySelector('.elements__item').cloneNode(true);
-  elementsTitle.textContent = inputElementTitle.value;
-  elementsLink.src = inputElementLink.value;
-  elementsLink.alt = `${elementsTitle}`; 
-  elementsContainer.prepend(elementsItem);
+  imageLink.addEventListener('click', () => openBigPicture({ name, link }));
+
+  const LikeBtn = elementsItem.querySelector('.elements__like-button');
+  LikeBtn.addEventListener('click', () => {
+    LikeBtn.classList.toggle('elements__like-button_active')
+  });
+
+  const elementDeleteBtn = elementsItem.querySelector('.elements__delete-button');
+  elementDeleteBtn.addEventListener('click', deleteItem);
+  function deleteItem(evt) {
+    evt.target.closest('.elements__item').remove()
+  }
+
   return elementsItem;
-};
+}
 
-//add template elements
-elementsContainer.append(...initialCards.map(createItem));
+// render Elements
+const renderElements = ({ name, link }) => {
+  elementsContainer.append(createItems({ name, link }))
+}
 
-//open big picture
-elementsLink.addEventListener('click', () => {
-  openBigPicture(imgTitle, imgLink);
-});
+elementsContainer.append(...initialCards.map(createItems));
 
- function openBigPicture(imgTitle, imgLink) {
-  bigPicture.src = elementsLink;
-  bigPictureCaption.textContent = elementsTitle;
-  bigPicture.alt = elementsTitle;
-  openPopup(openBigPicture);
-};
-
-// likes
-elementsLikeBtn.addEventListener('click', () => {
-  elementsLikeBtn.classList.toggle('.elements__like-button_active')
-});
-
-// delete picture
-elementsDelete.addEventListener('click', () => {
-  evt.preventDefault();
-  evt.target.closest('.elements__item').remove();
-});
-
-
-//initialCards.forEach(({ name, link }) => {
-//createCard(name, link)
-//})
+//BIG PICTURE
+function openBigPicture({ name, link }) {
+  bigPicture.src = link;
+  bigPictureCaption.textContent = name;
+  bigPicture.alt = name;
+  openPopup(popupBigPicture)
+}

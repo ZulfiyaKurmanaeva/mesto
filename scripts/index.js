cf381e6d5ccb946c7723c1bbb2c9a013d558e43a
Zulfiya Kurmanaeva <kurzul13@gmail.com>
2022-12-20 03:42:09 +0500

    mesto 20/12/22 03.40

--- scripts/index.js
+++ scripts/index.js
@@ -1,51 +1,22 @@
-const initialCards = [
-  {
-    name: 'Мост Золотые Ворота',
-    link: 'https://images.unsplash.com/photo-1664319744952-fc688e23c988?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
-  },
-  {
-    name: 'Венеция',
-    link: 'https://images.unsplash.com/photo-1666880780155-81bd7ff1008a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
-  },
-  {
-    name: 'Микены',
-    link: 'https://images.unsplash.com/photo-1670613873482-fdb5ea2fa6dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
-  },
-  {
-    name: 'Стокгольм',
-    link: 'https://images.unsplash.com/photo-1667382988243-6a1e5e85224e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
-  },
-  {
-    name: 'Тулуза',
-    link: 'https://images.unsplash.com/photo-1669854336002-0a2f4721dd5f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
-  },
-  {
-    name: 'Нью-Йорк',
-    link: 'https://images.unsplash.com/photo-1669058002610-f325ddddf4d6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
-  }
-];
-
 //POPUP
-const popup = document.querySelector('.popup');
-const closeButton = document.querySelector('.popup__close-button');
-const closeElementButton = document.querySelector('.popup__close-button_type_elements');
-const closeProfileButton = document.querySelector('.popup__close-button_type_profile');
-const closePictureButton = document.querySelector('.popup__close-button_type_picture');
+const elementCloseButton = document.querySelector('.popup__close-button_type_elements');
+const profileCloseButton = document.querySelector('.popup__close-button_type_profile');
+const pictureCloseButton = document.querySelector('.popup__close-button_type_picture');
 
 function openPopup(popup) {
   popup.classList.add('popup_opened');
 }
 
-closeElementButton.addEventListener('click', () => {
+//CLOSE POPUP
+elementCloseButton.addEventListener('click', () => {
   closePopup(popupAddElements);
 });
-closeProfileButton.addEventListener('click', () => {
+profileCloseButton.addEventListener('click', () => {
   closePopup(popupEditProfile);
 });
-closePictureButton.addEventListener('click', () => {
+pictureCloseButton.addEventListener('click', () => {
   closePopup(popupBigPicture);
 });
-
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
 }
@@ -76,11 +47,9 @@ popupEditProfile.addEventListener('submit', (evt) => {
 const popupAddElements = document.querySelector('.popup_type_add-elements'); //elements form
 const profileAddButton = document.querySelector('.profile__add-button');
 const elementsContainer = document.querySelector('.elements');
-const elementsTemplate = document.querySelector('#elementsTemplate');
-
+const elementsTemplate = document.querySelector('#elementsTemplate').content.querySelector('.elements__item');
 const titleInput = document.querySelector('.popup__input_type_elements-title');
 const linkInput = document.querySelector('.popup__input_type_elements-link');
-
 const popupBigPicture = document.querySelector('.popup_type_big-picture');
 const bigPicture = document.querySelector('.popup__big-picture');
 const bigPictureCaption = document.querySelector('.popup__picture-caption');
@@ -96,43 +65,44 @@ popupAddElements.addEventListener('submit', (evt) => {
   const link = linkInput.value;
   const name = titleInput.value;
   renderElements({ name, link });
-  linkInput.value = "";
-  titleInput.value = "";
   closePopup(popupAddElements);
   popupAddElements.reset();
 })
 
-//create Elements
-const createItems = ({ name, link }) => {
-  const elementsItem = elementsTemplate.content.querySelector('.elements__item').cloneNode(true);
+//create Element
+const createCard = ({ name, link }) => {
+  const elementsItem = elementsTemplate.cloneNode(true);
   const imageLink = elementsItem.querySelector('.elements__image');
   const imageTitle = elementsItem.querySelector('.elements__title');
+  const elementDeleteBtn = elementsItem.querySelector('.elements__delete-button');
+  const likeButton = elementsItem.querySelector('.elements__like-button');
+
   imageLink.src = link;
   imageTitle.textContent = name;
   imageLink.alt = name;
 
   imageLink.addEventListener('click', () => openBigPicture({ name, link }));
-
-  const LikeBtn = elementsItem.querySelector('.elements__like-button');
-  LikeBtn.addEventListener('click', () => {
-    LikeBtn.classList.toggle('elements__like-button_active')
-  });
-
-  const elementDeleteBtn = elementsItem.querySelector('.elements__delete-button');
-  elementDeleteBtn.addEventListener('click', deleteItem);
-  function deleteItem(evt) {
-    evt.target.closest('.elements__item').remove()
-  }
+  likeButton.addEventListener('click', () => makeLikeActive(likeButton));
+  elementDeleteBtn.addEventListener('click', deleteElement);
 
   return elementsItem;
 }
 
-// render Elements
-const renderElements = ({ name, link }) => {
-  elementsContainer.append(createItems({ name, link }))
+// RENDER ELEMENTS
+function renderElements({ name, link }) {
+  elementsContainer.prepend(createCard({ name, link }));
+}
+elementsContainer.append(...initialCards.map(renderElements));
+
+//DELETE ELEMENT
+function deleteElement(evt) {
+  evt.target.closest('.elements__item').remove();
 }
 
-elementsContainer.append(...initialCards.map(createItems));
+//LIKES
+function makeLikeActive(likeButton) {
+  likeButton.classList.toggle('elements__like-button_active');
+}
 
 //BIG PICTURE
 function openBigPicture({ name, link }) {
---
+++ scripts/initialcards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+    {
+        name: 'Рим, Италия',
+        link: 'https://images.unsplash.com/photo-1670792373724-39fd52df804e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
+    },
+    {
+        name: 'Венеция, Италия',
+        link: 'https://images.unsplash.com/photo-1666880780155-81bd7ff1008a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
+    },
+    {
+        name: 'Микены, Греция',
+        link: 'https://images.unsplash.com/photo-1670613873482-fdb5ea2fa6dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
+    },
+    {
+        name: 'Стокгольм, Швеция',
+        link: 'https://images.unsplash.com/photo-1667382988243-6a1e5e85224e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
+    },
+    {
+        name: 'Тулуза, Франция',
+        link: 'https://images.unsplash.com/photo-1669854336002-0a2f4721dd5f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
+    },
+    {
+        name: 'Нью-Йорк, США',
+        link: 'https://images.unsplash.com/photo-1669058002610-f325ddddf4d6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
+    }
+];

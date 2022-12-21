6897df8c4a723ff18af428c4437b50097ed0696d
ZulfiyaKurmanaeva <114097622+ZulfiyaKurmanaeva@users.noreply.github.com>
2022-12-21 12:37:16 +0300

    21/12/ 12/37

--- scripts/index.js
+++ scripts/index.js
@@ -1,49 +1,16 @@
-//POPUP
+// VARIABLES
+//popup
 const elementCloseButton = document.querySelector('.popup__close-button_type_elements');
 const profileCloseButton = document.querySelector('.popup__close-button_type_profile');
 const pictureCloseButton = document.querySelector('.popup__close-button_type_picture');
-
-function openPopup(popup) {
-  popup.classList.add('popup_opened');
-}
-
-//CLOSE POPUP
-elementCloseButton.addEventListener('click', () => {
-  closePopup(popupAddElements);
-});
-profileCloseButton.addEventListener('click', () => {
-  closePopup(popupEditProfile);
-});
-pictureCloseButton.addEventListener('click', () => {
-  closePopup(popupBigPicture);
-});
-function closePopup(popup) {
-  popup.classList.remove('popup_opened');
-}
-
-//PROFILE
+//profile
 const popupEditProfile = document.querySelector('.popup_type_edit-profile');
 const profileEditButton = document.querySelector('.profile__edit-button');
 const inputName = document.querySelector('.popup__input_type_name');
 const inputAbout = document.querySelector('.popup__input_type_about');
 const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__about');
-//open edit profile
-profileEditButton.addEventListener('click', () => {
-  inputName.value = profileName.textContent;
-  inputAbout.value = profileAbout.textContent;
-  openPopup(popupEditProfile);
-});
-
-//save profile
-popupEditProfile.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  profileName.textContent = inputName.value;
-  profileAbout.textContent = inputAbout.value;
-  closePopup(popupEditProfile);
-})
-
-//ELEMENTS
+//elements
 const popupAddElements = document.querySelector('.popup_type_add-elements'); //elements form
 const profileAddButton = document.querySelector('.profile__add-button');
 const elementsContainer = document.querySelector('.elements');
@@ -54,22 +21,15 @@ const popupBigPicture = document.querySelector('.popup_type_big-picture');
 const bigPicture = document.querySelector('.popup__big-picture');
 const bigPictureCaption = document.querySelector('.popup__picture-caption');
 
-//open Elements popup
-profileAddButton.addEventListener('click', () => {
-  openPopup(popupAddElements)
-});
-
-//submit Elements
-popupAddElements.addEventListener('submit', (evt) => {
-  evt.preventDefault()
-  const link = linkInput.value;
-  const name = titleInput.value;
-  renderElements({ name, link });
-  closePopup(popupAddElements);
-  popupAddElements.reset();
-})
-
-//create Element
+//FUNCTIONS
+//popup
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+}
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+//create Elements
 const createCard = ({ name, link }) => {
   const elementsItem = elementsTemplate.cloneNode(true);
   const imageLink = elementsItem.querySelector('.elements__image');
@@ -87,27 +47,73 @@ const createCard = ({ name, link }) => {
 
   return elementsItem;
 }
-
-// RENDER ELEMENTS
+// render
 function renderElements({ name, link }) {
   elementsContainer.prepend(createCard({ name, link }));
 }
-elementsContainer.append(...initialCards.map(renderElements));
+initialCards.forEach(renderElements);
 
-//DELETE ELEMENT
+//delete element
 function deleteElement(evt) {
   evt.target.closest('.elements__item').remove();
 }
 
-//LIKES
+//likes
 function makeLikeActive(likeButton) {
   likeButton.classList.toggle('elements__like-button_active');
 }
 
-//BIG PICTURE
+//open picture
 function openBigPicture({ name, link }) {
   bigPicture.src = link;
   bigPictureCaption.textContent = name;
   bigPicture.alt = name;
   openPopup(popupBigPicture)
 }
+
+//EVENTS
+//close popup
+elementCloseButton.addEventListener('click', () => {
+  closePopup(popupAddElements);
+});
+profileCloseButton.addEventListener('click', () => {
+  closePopup(popupEditProfile);
+});
+pictureCloseButton.addEventListener('click', () => {
+  closePopup(popupBigPicture);
+});
+
+
+//open edit profile
+profileEditButton.addEventListener('click', () => {
+  inputName.value = profileName.textContent;
+  inputAbout.value = profileAbout.textContent;
+  openPopup(popupEditProfile);
+});
+
+//save profile
+popupEditProfile.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileAbout.textContent = inputAbout.value;
+  closePopup(popupEditProfile);
+})
+
+//open Elements popup
+profileAddButton.addEventListener('click', () => {
+  openPopup(popupAddElements)
+});
+
+//submit Elements
+popupAddElements.addEventListener('submit', (evt) => {
+  evt.preventDefault()
+  const link = linkInput.value;
+  const name = titleInput.value;
+  renderElements({ name, link });
+  closePopup(popupAddElements);
+  popupAddElements.reset();
+})
+
+
+
+

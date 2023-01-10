513da174590193eeecfcd8e95a70c113aaa1defe
ZulfiyaKurmanaeva <114097622+ZulfiyaKurmanaeva@users.noreply.github.com>
2023-01-10 15:50:44 +0300

    10/01 15.50

--- scripts/validate.js
+++ scripts/validate.js
@@ -2,9 +2,9 @@
     formSelector: '.popup__form',
     inputSelector: '.popup__input',
     submitButtonSelector: '.popup__button',
-    inactiveButtonClass: 'popup__button_disabled',
-    inputErrorClass: 'popup__input_type_error',
-    errorClass: 'popup__error_visible'
+    inactiveButtonClass: '.popup__button_disabled',
+    inputErrorClass: '.popup__input_type_error',
+    errorClass: '.popup__error_visible'
   };  
 
 function showInputError(formElement, inputElement, config) {
